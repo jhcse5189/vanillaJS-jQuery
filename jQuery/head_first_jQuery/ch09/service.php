637f4cbfd5492264c3c7316b80c6f213d9f4fed1
2018009143 <jhcse5189@gmail.com>
2020-01-26 00:33:18 +0900

    ch09/ service.php? post test

--- jQuery/head_first_jQuery/ch09/service.php
+++ jQuery/head_first_jQuery/ch09/service.php
@@ -1,6 +1,40 @@
 <?php
 
-if ($_GET['action'] == 'getRunners') {
+if ($_POST['action'] == 'addRunner') {
+    $fname = htmlspecialchars($_POST['txtFirstName']);
+    $lname = htmlspecialchars($_POST['txtLastName']);
+    $gender = htmlspecialchars($_POST['ddlGender']);
+    $minutes = htmlspecialchars($_POST['txtMinutes']);
+    $seconds = htmlspecialchars($_POST['txtSeconds']);
+
+    // prevent to input special characters and numbers for name
+    if (preg_match('/[^a-zA-Z\s]/g', $fname) || preg_match('/[^a-zA-Z\s]/g', $lname)) {
+        fail('Invalid name provided.');
+    }
+
+    // only numbers for finish time
+    if (preg_match('/^[0-9]*/g', $minutes) || preg_match('/^[0-9]*/g', $seconds)) {
+        fail('Invaild finish time provided.');
+    }
+
+    // not allow any empty input
+    if (empty($fname) || empty($lname) || empty($gender) || empty($minutes) || empty($seconds)) {
+        fail('There are empty input on your form.');
+    }
+
+    $time = $minutes . ":" . $seconds;
+    $sql = "INSERT INTO runners (first_name, last_name, gender, finish_time)
+            VALUES ($fname, $lname, $gender, $time);";
+    $result = db_query($sql);
+
+    if ($result) {
+        $msg = "Runner: " . $fname . " " . $lname . " added successfully.";
+        success($msg);
+    } else {
+        fail('Insert failed.');
+    }
+
+} else if ($_GET['action'] == 'getRunners') {
     $sql = "SELECT first_name, last_name, gender, finish_time FROM runners ORDER BY finish_time";
     $result = db_query($sql);
     
@@ -15,7 +49,7 @@ mysqli_close($connect);
 
 function db_query($sql) {
     $connect = mysqli_connect("localhost", "root", "bammerdb", "hfjq_race_info")
-    OR die(fail('disconnected.'));
+    OR die(fail('Disconnected to MariaDB'));
 
     return mysqli_query($connect, $sql);
 }

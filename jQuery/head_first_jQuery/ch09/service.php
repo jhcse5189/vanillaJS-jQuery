f25cd33df00538abc56f6098b3ee052745f12b24
2018009143 <jhcse5189@gmail.com>
2020-01-25 22:03:30 +0900

    ch09/service.php connect & query => db_query()

--- jQuery/head_first_jQuery/ch09/service.php
+++ jQuery/head_first_jQuery/ch09/service.php
@@ -1,17 +1,17 @@
 <?php
-$connect = mysqli_connect("localhost", "root", "bammerdb", "hfjq_race_info");
-
-if (!$connect) {
-    echo "ERROR:)" . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    exit;
-}
 
 $sql = "SELECT first_name, last_name, gender, finish_time FROM runners ORDER BY finish_time";
-$result = mysqli_query($connect, $sql);
+$result = db_query($sql);
 
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
     print_r($row);
 };
 
 mysqli_close($connect);
+
+function db_query($sql) {
+    $connect = mysqli_connect("localhost", "root", "bammerdb", "hfjq_race_info")
+    OR die ('disconnected.');
+
+    return mysqli_query($connect, $sql);
+}

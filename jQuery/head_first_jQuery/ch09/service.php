cb3c2596e3f9958174fb2983221db10d41d81b2c
2018009143 <jhcse5189@gmail.com>
2020-01-25 21:56:12 +0900

    ch09/service.php fetch_array test

--- jQuery/head_first_jQuery/ch09/service.php
+++ jQuery/head_first_jQuery/ch09/service.php
@@ -5,18 +5,13 @@ if (!$connect) {
     echo "ERROR:)" . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     exit;
-} 
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL . "<br>";
-echo "Host information: " . mysqli_get_host_info($connect) . PHP_EOL . "<br>";
-
-$sql = "SELECT * FROM runners";
-echo $sql . "<br>";
-
-$result = mysqli_query($connect, $sql);
-if ($result) {
-    echo "Success to insert the record!" . "<br>";
-} else {
-    echo "Fail to select:)" . "<br>";
 }
+
+$sql = "SELECT first_name, last_name, gender, finish_time FROM runners ORDER BY finish_time";
+$result = mysqli_query($connect, $sql);
+
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    print_r($row);
+};
 
 mysqli_close($connect);

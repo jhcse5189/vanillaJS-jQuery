32b5236b19dbd7c1e1b7ce7ac854ae01263b0ccf
Bammer <jhcse5189@gmail.com>
2022-04-16 14:46:37 +0000

    Add. AJAX-XML calling routine in ./script.js - Mod. file's datatype .xml to .json later - Init. .xml data

--- ajax/projects/auto_update_board/src/js/script.js
+++ ajax/projects/auto_update_board/src/js/script.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
 
+    const FREQ = 10000;
+    let repeat = true;
+
     function showFrequency() {
-        $("#freq").html("Page refreshes every 10 second(s).");
+        $("#freq").html("Page refreshes every " + FREQ/1000 + " second(s).");
+    }
+
+    function startAJAXcalls() {
+        if (repeat) {
+            setTimeout(function() {
+                getXMLPaticipants();
+                startAJAXcalls();
+            }, FREQ);
+        }
+    }
+
+    function getXMLPaticipants() {
+        $.ajax({
+            url: "records.xml",
+            cache: false, // deny browser cache
+            dataType: "xml",
+            success: function(xml) {
+                console.log(xml);
+            }
+        });
     }
 
     $("#btnStart").click(function(){
@@ -13,4 +36,6 @@ $(document).ready(function(){
     });
 
     showFrequency();
+    getXMLPaticipants();
+    startAJAXcalls();
 });

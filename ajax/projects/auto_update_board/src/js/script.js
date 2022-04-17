c8ecc3fec27ce33c3d3e49a7d68931d3f8d04691
Bammer <jhcse5189@gmail.com>
2022-04-17 14:46:46 +0000

    Imp. sorted append & Del. li::marker

--- ajax/projects/auto_update_board/src/js/script.js
+++ ajax/projects/auto_update_board/src/js/script.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
     const FREQ = 10000;
     let repeat = true;
 
+    let Participant = function(name, gender, time) {
+        this.name = name;
+        this.gender = gender;
+        this.time = time;
+    }
+
     function showFrequency() {
         $("#freq").html("Page refreshes every " + FREQ/1000 + " second(s).");
     }
@@ -28,27 +34,57 @@ $(document).ready(function(){
                 $("#records_f").empty();
                 $("#records_all").empty();
 
+                let participants = [];
+
                 // parse XML
                 $(xml).find("participant").each(function(){
 
+                    var n = $(this).find("name").text();
+                    var g = $(this).find("gender").text();
                     var t = parseInt($(this).find("time").text());
-                    
-                    // TODO : implement time parsing at other method
+
+                    let p = new Participant(n, g, t);
+                    participants.push(p);
+                });
+
+                // to use sorted append
+                participants.sort(function(a, b){
+
+                    if (a.time == b.time)
+                        return a.name < b.name ? -1 : (a.name > b.name ? 1 : 0);
+                
+                    return a.time - b.time;
+                });
+
+                $.each(participants, function(key, value){
+    
+                    var n = participants[key]["name"];
+                    var g = participants[key]["gender"];
+                    var t = participants[key]["time"];
+
+                    // time parsing
                     var m = Math.floor(t / 60);
                     var s = t % 60;
 
                     if (m.toString().length == 1) { m = "0" + m; }
                     if (s.toString().length == 1) { s = "0" + s; }
 
-                    var info = `<li>${$(this).find("name").text()} (${m}:${s})</li>`;
-
-                    // TODO : implement sorted append
-                    if ($(this).find("gender").text() == "m") {
+                    // sorted append
+                    if (g == "m") {
+                        var idx = ($("#records_m").children().length + 1).toString();
+                        var info = `<li>${idx}. ${n} (${m}:${s})</li>`;
+                    
                         $("#records_m").append(info);
-                    } else if ($(this).find("gender").text() == "f") {
+                    } else if (g == "f") {
+                        var idx = ($("#records_f").children().length + 1).toString();
+                        var info = `<li>${idx}. ${n} (${m}:${s})</li>`;
+                    
                         $("#records_f").append(info);
                     }
 
+                    var idx = ($("#records_all").children().length + 1).toString();
+                    var info = `<li>${idx}. ${n} (${m}:${s})</li>`;
+                
                     $("#records_all").append(info);
                 });
             }
